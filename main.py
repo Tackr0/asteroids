5fbc337b55de1ad7a1b9c3d4717247994341c7c5
Tackro <dnews@btinternet.com>
2024-12-27 22:17:46 +0000

    - Updated main.py to use pygame loading only basic background and to respect exit window event. - Added .gitignore for pycache and virtual environment - Added constants file to import from

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
-import pygame
+import pygame # type: ignore
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #Game updates every iteration
+    while True:
+        #Paint background of game black
+        screen.fill("black")
+        
+        #Exit if user closes via x button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+
+        pygame.display.flip()
+
 
 #Supposedly runs only when file is run directly
 if __name__ == "__main__":
