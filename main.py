cb3c80eed3fe964978407d8747afa6205c09f2d2
Tackro <dnews@btinternet.com>
2024-12-28 00:57:11 +0000

    - Added Circle Shape Class for essentially all sprites to inherit from - Added Player Class with rendering functionality - Updated main.py and constants.py to accommodate above changes

--- main.py
+++ main.py
@@ -1,22 +1,32 @@
 import pygame # type: ignore
+
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #Control game speed
+    clock = pygame.time.Clock()
+    dt = 0
+    FRAMERATE = 60
+
+    #Instantiates player in middle of screen
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     #Game updates every iteration
     while True:
-        #Paint background of game black
-        screen.fill("black")
-        
         #Exit if user closes via x button
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #Paint elements on screen
+        screen.fill("black")
+        player.draw(screen)
 
-
-
+        dt = clock.tick(FRAMERATE) /1000
         pygame.display.flip()
 
 
